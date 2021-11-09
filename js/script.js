86af2024c05a5b65741cf8e2de8a4b348bcf0247
Antonioangy <dumitruangy26@gmail.com>
2021-11-09 17:23:31 +0100

    creo arrey

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// genero un array di numeri casuali
+const numArrey = [];
+
+for (numArrey.length<5){
+    let RandomNum = Math.floor(math.random() * 100);
+
+    console.log(numArrey)
+}
+
+// faccio apparire sullo schermo 5 numeri casuali per 30 secondi
+
+// chiedo all'utente di mettere i numeri che ha visto precedentemente sul display
